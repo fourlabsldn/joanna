b1a9d759df8dc15ff92ae03f3e679cf848ca2ebe
lazamar <lazaronijunior@gmail.com>
2016-10-14 17:59:21 +0100

    Add content and styling

--- gulpfile.js
+++ gulpfile.js
@@ -12,12 +12,12 @@ organiser.registerAll('./tasks', {
   },
   'browser-sync': {
     src: '.', // it doesn't matter, it's just so the task object is not ignored.
-    reloadOn: ['transpile-react'], // reload page when these tasks happen
+    reloadOn: ['sass', 'copy-static'], // reload page when these tasks happen
     startPath: 'index.html',
     baseDir: './',
   },
   'copy-static': {
-    src,
+    src: path.join(src, '**/*'),
     dest,
   },
 });
