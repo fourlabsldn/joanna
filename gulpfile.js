d54867b20f275b030470284a27bb05f7728c47e4
lazamar <lazaronijunior@gmail.com>
2016-10-14 17:02:42 +0100

    Add all them tasks

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+// List all available tasks
+
+const src = 'src';
+const dest = 'dist';
+const path = require('path');
+
+const organiser = require('gulp-organiser');
+organiser.registerAll('./tasks', {
+  sass: {
+    src: path.join(src, 'styles/**/*.scss'),
+    dest: path.join(dest, 'styles'),
+  },
+  'browser-sync': {
+    src: '.', // it doesn't matter, it's just so the task object is not ignored.
+    reloadOn: ['transpile-react'], // reload page when these tasks happen
+    startPath: 'index.html',
+    baseDir: './',
+  },
+  'copy-static': {
+    src,
+    dest,
+  },
+});
